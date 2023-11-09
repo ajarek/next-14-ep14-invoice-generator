91fb1b168ff865dd69bac2507cf78000989502c0
Ajarek <ajarek@poczta.onet.pl>
2023-11-09 23:51:49 +0100

    added localStorage

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import {
@@ -11,37 +11,81 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from '@/components/ui/table'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 
+import { saveStorage } from '@/lib/localStorage'
 const Row = () => {
-  const [nr,setNr]=useState<string>()
-  const [name,setName]=useState<string>()
-  const [quantity,setQuantity]=useState<string>()
-  const [price,setPrice]=useState<string>()
-  const [vat,setVat]=useState<string>()
+  const [nr, setNr] = useState<string>()
+  const [name, setName] = useState<string>()
+  const [quantity, setQuantity] = useState<string>()
+  const [price, setPrice] = useState<string>()
+  const [vat, setVat] = useState<string>()
 
-  const dataRow={nr,name,quantity,price,vat}
- 
+  let rowPrice = Number(quantity) * Number(price) * (1 + Number(vat) / 100) || 0
 
-  const rowPrice= Number(quantity)*Number(price)*(1+Number(vat)/100)||0
-  
+  useEffect(() => {
+    const dataRow = { nr, name, quantity, price, vat, rowPrice }
+    if (rowPrice !== 0) {
+      saveStorage(dataRow, 'ArrayRow')
+    }
+  }, [rowPrice, name, nr, price, quantity, vat])
+
   return (
-    
-    
     <>
-      
-        <TableCell><Input onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setNr(event.target.value)}/></TableCell>
-        <TableCell><Input onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setName(event.target.value)}/></TableCell>
-        <TableCell><Input onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setQuantity(event.target.value)}/></TableCell>
-        <TableCell><Input onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setPrice(event.target.value)}/></TableCell>
-        <TableCell><Input onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setVat(event.target.value)}/></TableCell>
-        <TableCell>{rowPrice.toFixed(2)}</TableCell>
-        <TableCell><Button>Usuń</Button></TableCell>
-        <TableCell><Button className='bg-transparent hover:bg-transparent'><Input type='checkbox' className='w-[30px] h-[30px] ' /></Button></TableCell>
-     
-        </>
-  
-  
+      <TableCell>
+        <Input
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setNr(event.target.value)
+          }
+        />
+      </TableCell>
+      <TableCell>
+        <Input
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setName(event.target.value)
+          }
+        />
+      </TableCell>
+      <TableCell>
+        <Input
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setQuantity(event.target.value)
+          }
+        />
+      </TableCell>
+      <TableCell>
+        <Input
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setPrice(event.target.value)
+          }
+        />
+      </TableCell>
+
+      <TableCell>
+        <Select onValueChange={(event) => setVat(event)}>
+          <SelectTrigger className='w-[150px]'>
+            <SelectValue placeholder='Wybierz VAT' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='7'>7</SelectItem>
+            <SelectItem value='23'>23</SelectItem>
+            <SelectItem value='0'>0</SelectItem>
+          </SelectContent>
+        </Select>
+      </TableCell>
+
+      <TableCell>{rowPrice.toFixed(2)}</TableCell>
+      <TableCell>
+        <Button>Usuń</Button>
+      </TableCell>
+    </>
   )
 }
 
